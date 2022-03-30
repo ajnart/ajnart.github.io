90ea780b7e25d3ef13008b98401f32aa4954caaa
Aj - Thomas <thomascamlong@gmail.com>
2022-03-30 12:16:01 +0200

    Update no-print

--- plugins/particles/particles.js
+++ plugins/particles/particles.js
@@ -1491,7 +1491,7 @@ window.particlesJS = function(tag_id, params){
 
   /* pJS elements */
   var pJS_tag = document.getElementById(tag_id),
-      pJS_canvas_class = 'particles-js-canvas-el',
+      pJS_canvas_class = 'particles-js-canvas-el no-print',
       exist_canvas = pJS_tag.getElementsByClassName(pJS_canvas_class);
 
   /* remove canvas if exists into the pJS target tag */
